ad7adc468cd5049f9610f790e9b61e87dcb44ab0
Kevin Zeidler <kzeidler@gmail.com>
2021-11-28 12:00:49 -0800

    initial commit

--- babynames/manage.py
+++ babynames/manage.py
@@ -15,6 +15,18 @@ def hello():
     result = db.engine.execute("select * from NationalNames LIMIT 5;")
     print(result.fetchall())
 
+@manager.command
+def db_shell():
+    while True:
+        try:
+            cmd = input("sql> ")
+            if cmd == "exit":
+                break
+            result = db.engine.execute(cmd)
+            print(result.fetchall())
+        except Exception as e:
+            print(f"{e.__class__.__name__} while executing {cmd} ::  {e}. Check your syntax and make sure your identifiers correctly refer to the columns defined in the table schema.")
+
 
 if __name__ == "__main__":
     manager.run()
